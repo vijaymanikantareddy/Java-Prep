767a136d75a61ff069f41c7f1a1623a1d3021a36
vijaymanikantareddy <svmreddy7799@gmail.com>
2025-04-15 13:28:54 +0530

    DataTypes

---
+++ eclipse-workspace/DataTypes/src/com/datatypes/Test.java
@@ -0,0 +1,26 @@
+package com.datatypes;
+
+public class Test {
+	public static void main(String[] args) {
+		byte byteVariable = 12;
+		short shortVariable = 12425;
+		int intVariable = 98883;
+		long longVariable = 123110390L;
+
+		float floatVariable = 113.1234567891f;
+		double doubleVariable = 12345.123456789012345678;
+
+		boolean booleanVariable = true;
+
+		char charVariable = 'A';
+
+		System.out.println(byteVariable);
+		System.out.println(shortVariable);
+		System.out.println(intVariable);
+		System.out.println(longVariable);
+		System.out.println(floatVariable);
+		System.out.println(doubleVariable);
+		System.out.println(booleanVariable);
+		System.out.println(charVariable);
+	}
+}
