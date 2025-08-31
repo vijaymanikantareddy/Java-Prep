34773dffaa77385c711cf05e6c08b7b4458cd315
vijaymanikantareddy <svmreddy7799@gmail.com>
2025-08-31 15:04:48 +0530

    Spring Core NoXML

---
+++ eclipse-workspace/SpringCoreNoXML/src/main/java/com/Address.java
@@ -0,0 +1,43 @@
+package com;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Address {
+	@Value("${addrId}")
+	private int addrId;
+	@Value("${street}")
+	private String street;
+
+	public Address() {
+
+	}
+
+	@Override
+	public String toString() {
+		return "Address [addrId=" + addrId + ", street=" + street + "]";
+	}
+
+	public Address(int addrId, String street) {
+		super();
+		this.addrId = addrId;
+		this.street = street;
+	}
+
+	public int getAddrId() {
+		return addrId;
+	}
+
+	public void setAddrId(int addrId) {
+		this.addrId = addrId;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+}
---
+++ eclipse-workspace/SpringCoreNoXML/src/main/java/com/Student.java
@@ -0,0 +1,60 @@
+package com;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component("student")
+public class Student {
+	
+	@Value("${id}")
+	private int studentid;
+	
+	@Value("${name}")
+	private String name;
+	
+	
+	private Address addr;
+
+	public Student(int studentid, String name, Address addr) {
+		super();
+		this.studentid = studentid;
+		this.name = name;
+		this.addr = addr;
+	}
+
+	public Address getAddr() {
+		return addr;
+	}
+	
+	@Autowired
+	public void setAddr(Address addr) {
+		this.addr = addr;
+	}
+
+	public Student() {
+
+	}
+
+	public int getStudentid() {
+		return studentid;
+	}
+
+	public void setStudentid(int studentid) {
+		this.studentid = studentid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [studentid=" + studentid + ", name=" + name + ", address=" + addr + "]";
+	}
+
+}
