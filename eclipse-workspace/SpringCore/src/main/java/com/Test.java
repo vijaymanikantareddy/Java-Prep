06b8e4410ea59d49149d0f05e948322b65e0accf
vijaymanikantareddy <svmreddy7799@gmail.com>
2025-09-01 11:31:58 +0530

    Scope of Beans

--- eclipse-workspace/SpringCore/src/main/java/com/Test.java
+++ eclipse-workspace/SpringCore/src/main/java/com/Test.java
@@ -7,12 +7,17 @@ public class Test {
 	public static void main(String[] args) {
 
 		ApplicationContext container = new ClassPathXmlApplicationContext("beans.xml");
-		System.out.println("Context Loaded");
+//		System.out.println("Context Loaded");
+//
+//		studentDetails(container);
+//		
+//		employeeDetails(container);
 
-		studentDetails(container);
-		
-		employeeDetails(container);
-
+		User user1 = container.getBean("user", User.class);
+		User user2 = container.getBean("user", User.class);
+		System.out.println(user1);
+		System.out.println(user2);
+		System.out.println(user1 == user2);
 	}
 
 	private static void employeeDetails(ApplicationContext container) {
@@ -23,7 +28,7 @@ public class Test {
 	private static void studentDetails(ApplicationContext container) {
 		Student student = container.getBean("st", Student.class);
 		System.out.println(student.getAddr());
-		
+
 //		Student student2 = container.getBean("st2", Student.class);
 //		System.out.println(student2);
 //		System.out.println(student == student2);
