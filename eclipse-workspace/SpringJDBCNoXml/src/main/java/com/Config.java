7a2a73fe0f03296f0ae7870dd3d934de7a67a25f
vijaymanikantareddy <svmreddy7799@gmail.com>
2025-09-03 11:11:48 +0530

    Spring JDBC No XML

---
+++ eclipse-workspace/SpringJDBCNoXml/src/main/java/com/Config.java
@@ -0,0 +1,22 @@
+package com;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class Config {
+	@Bean("template")
+	public JdbcTemplate getJdbcTemplate() {
+		DataSource dataSource = getDataSource();
+		return new JdbcTemplate(dataSource);
+	}
+
+	@Bean("dmds")
+	public DataSource getDataSource() {
+		return new DriverManagerDataSource("jdbc:mysql://localhost:3306/mydb", "root", "root");
+	}
+}
---
+++ eclipse-workspace/SpringJDBCNoXml/src/main/java/com/Test.java
@@ -0,0 +1,53 @@
+package com;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		ApplicationContext container = new AnnotationConfigApplicationContext(Config.class);
+		JdbcTemplate template = container.getBean("template", JdbcTemplate.class);
+//		template.update("insert into employees values(?,?,?) ", 20, "nineteen@gmail.com", 19000);
+//		template.update("update employees set email=? where empid=?", "updated@gmail.com", 20);
+//		template.update("delete from employees where empid=?", 20);
+//		System.out.println("Query executed");
+
+//		// RowMapper
+		Employee employee = template.queryForObject("Select * from employees where empId=3", new RowMapper<Employee>() {
+			@Override
+			public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new Employee(rs.getInt(1), rs.getString(2), rs.getInt
+		(3));
+			}
+		});
+
+		System.out.println(employee);
+//
+//		// get all data
+		List<Employee> employeesList = template.query("select * from employees", new RowMapper<Employee>() {
+			@Override
+			public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new Employee(rs.getInt(1), rs.getString(2), rs.getInt(3));
+			}
+		});
+
+		System.out.println(employeesList);
+//
+//		// using lambda function
+//		List<Employee> employeesList2 = template.query("Select * from Employees",
+//				(rs, num) -> new Employee(rs.getInt(1), rs.getString(2), rs.getInt(3)));
+//
+//		System.out.println(employeesList2);
+	}
+
+}
