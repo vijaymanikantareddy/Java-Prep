107653bb4984ef04205c98aca7a8bc1273a53c5d
vijaymanikantareddy <svmreddy7799@gmail.com>
2025-09-30 13:00:09 +0530

    First Spring Project

---
+++ STS-workspace/MyFirstProject/src/main/java/com/flm/controller/HiController.java
@@ -0,0 +1,13 @@
+package com.flm.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+	@GetMapping("/hi")
+	public String hi() {
+		return "Hello FLM";
+	}
+}
--- STS-workspace/demo1/src/main/java/com/example/demo/HiController.java
+++ STS-workspace/demo1/src/main/java/com/example/demo/HiController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HiController {
 	@RequestMapping("/hi")
 	public String hi() {
-		return "Hi";
+		return "Hi Hello Bye!!!";
 	}
 
 }
